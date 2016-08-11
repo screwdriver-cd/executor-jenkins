69e043323e3ebb738c1db2691527b2ed19fdd6ce
Min Zhang <minzhang@andrew.cmu.edu>
2016-08-11 16:41:05 -0700

    add stop method

--- test/index.test.js
+++ test/index.test.js
@@ -17,8 +17,6 @@ describe('index', () => {
     let jenkinsMock;
     let breakerMock;
     let BreakerFactory;
-    let createOpts;
-    let buildOpts;
 
     const config = {
         buildId: '4b8d9b530d2e5e297b4f470d5b0a6e1310d29c5e',
@@ -28,6 +26,18 @@ describe('index', () => {
     };
 
     const jobName = config.buildId;
+
+    const buildIdConfig = {
+        buildId: jobName
+    };
+
+    const fakeJobInfo = {
+        lastBuild: {
+            number: 1
+        }
+    };
+
+    const buildNumber = fakeJobInfo.lastBuild.number;
 
     before(() => {
         mockery.enable({
@@ -52,7 +62,19 @@ describe('index', () => {
             runCommand: sinon.stub()
         };
 
+        BreakerFactory = sinon.stub().returns(breakerMock);
+
         mockery.registerMock('fs', fsMock);
+        mockery.registerMock('circuit-fuses', BreakerFactory);
+
+        // eslint-disable-next-line global-require
+        Executor = require('../index');
+
+        executor = new Executor({
+            host: 'jenkins',
+            username: 'admin',
+            password: 'fakepassword'
+        });
     });
 
     afterEach(() => {
@@ -66,34 +88,16 @@ describe('index', () => {
 
     it('extends base class', () => {
         // eslint-disable-next-line global-require
-        Executor = require('../index');
-
-        executor = new Executor({
-            host: 'jenkins',
-            username: 'admin',
-            password: 'fakepassword'
-        });
-
-        // eslint-disable-next-line global-require
         const BaseExecutor = require('../node_modules/screwdriver-executor-base/index');
 
         assert.instanceOf(executor, BaseExecutor);
     });
 
     describe('start', () => {
+        let createOpts;
+        let buildOpts;
+
         beforeEach(() => {
-            BreakerFactory = sinon.stub().returns(breakerMock);
-            mockery.registerMock('circuit-fuses', BreakerFactory);
-
-            // eslint-disable-next-line global-require
-            Executor = require('../index');
-
-            executor = new Executor({
-                host: 'jenkins',
-                username: 'admin',
-                password: 'fakepassword'
-            });
-
             createOpts = {
                 module: 'job',
                 action: 'create',
@@ -146,7 +150,7 @@ describe('index', () => {
         });
 
         it('return error when job.build is getting error', (done) => {
-            const error = new Error('job.create error');
+            const error = new Error('job.build error');
 
             fsMock.readFile.yieldsAsync(null, TEST_XML);
             breakerMock.runCommand.withArgs(createOpts).yieldsAsync(null, null);
@@ -159,8 +163,80 @@ describe('index', () => {
         });
     });
 
+    describe('stop', () => {
+        let getOpts;
+        let stopOpts;
+
+        beforeEach(() => {
+            getOpts = {
+                module: 'job',
+                action: 'get',
+                params: [jobName]
+            };
+
+            stopOpts = {
+                module: 'build',
+                action: 'stop',
+                params: [jobName, buildNumber]
+            };
+        });
+
+        it('return null when the build is successfully stopped', (done) => {
+            breakerMock.runCommand.withArgs(getOpts).yields(null, fakeJobInfo);
+            breakerMock.runCommand.withArgs(stopOpts).yields(null);
+
+            executor.stop(buildIdConfig, (err) => {
+                assert.isNull(err);
+                assert.calledWith(breakerMock.runCommand, getOpts);
+                assert.calledWith(breakerMock.runCommand, stopOpts);
+                done();
+            });
+        });
+
+        it('return error when there is no build to be stopped yet', (done) => {
+            const noBuildJobInfo = {
+                lastBuild: null
+            };
+
+            breakerMock.runCommand.withArgs(getOpts).yields(null, noBuildJobInfo);
+            breakerMock.runCommand.withArgs(stopOpts).yields(null);
+
+            executor.stop(buildIdConfig, (err) => {
+                assert.deepEqual(err.message, 'No build has been started yet, try later');
+                done();
+            });
+        });
+
+        it('return error when job.get is getting error', (done) => {
+            const error = new Error('job.get error');
+
+            breakerMock.runCommand.withArgs(getOpts).yieldsAsync(error);
+            breakerMock.runCommand.withArgs(stopOpts).yields(null);
+
+            executor.stop(buildIdConfig, (err) => {
+                assert.deepEqual(err, error);
+                done();
+            });
+        });
+
+        it('return error when build.stop is getting error', (done) => {
+            const error = new Error('build.stop error');
+
+            breakerMock.runCommand.withArgs(getOpts).yields(null, fakeJobInfo);
+            breakerMock.runCommand.withArgs(stopOpts).yields(error);
+
+            executor.stop(buildIdConfig, (err) => {
+                assert.deepEqual(err, error);
+                done();
+            });
+        });
+    });
+
     describe('run without Mocked Breaker', () => {
         beforeEach(() => {
+            mockery.deregisterMock('circuit-fuses');
+            mockery.resetCache();
+
             // eslint-disable-next-line global-require
             Executor = require('../index');
 
